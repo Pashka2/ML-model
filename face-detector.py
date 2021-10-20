36ac208405deac339d8c6c976b04057047ab3ba3
Pasha <pashasachivichik@gmail.com>
2021-10-20 18:59:35 -0400

    was working on different IDE, switching back on pycharm

--- face-detector.py
+++ face-detector.py
@@ -1,16 +1,22 @@
 import cv2
+import numpy as np
 
 #load some pretrained data on face frontels from opencv ( haar cascade algorithm)
-trained_face_data = cv2.CascadeClassifier('harrcascade_frontalface_default.xml')
-
-#filename = "C:\Users\pasha\OneDrive\Desktop\PYTHON PROGRAMS\ML model\rdj.jpg"
+trained_face_data = cv2.CascadeClassifier("C:\\Users\\pasha\\AppData\\Local\\Programs\\Python\\Python39\\Lib\\site-packages\\cv2\\data\\harrcascade_frontalface_default.xml")
 
 #choose an image to detect faces in
-img = cv2.imread("rdj face.jpg")
+img = cv2.imread('C:/Users/pasha/OneDrive/Desktop/PYTHON PROGRAMS/ML model/download.jpg')
 
+#must convert to grayscale
+grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+# Detect faces
+face_corrdinates = trained_face_data.detectMultiScale(grayscaled_img)
+print(face_corrdinates)
 
-cv2.imshow("Pasha ",img)
-
+#
+#cv2.imshow("Face detector ",grayscaled_img)
 cv2.waitKey()
+
+
 print("code completed")
